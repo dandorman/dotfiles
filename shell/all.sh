5eff09e58172c4c011a6754ad5b05c628babbb59
Dan Dorman <dan.dorman@gmail.com>
2012-08-31 18:17:56 -0600

    Comment out JSTestDriver stuff for now.

--- shell/all.sh
+++ shell/all.sh
@@ -4,7 +4,7 @@ source $HOME/dotfiles/shell/ah.sh
 source $HOME/dotfiles/shell/alias.sh
 source $HOME/dotfiles/shell/config-bin.sh
 source $HOME/dotfiles/shell/growl.sh
-source $HOME/dotfiles/shell/js-test-driver.sh
+# source $HOME/dotfiles/shell/js-test-driver.sh
 source $HOME/dotfiles/shell/path.sh
 source $HOME/dotfiles/shell/rbenv.sh
 source $HOME/dotfiles/shell/ruby.sh
