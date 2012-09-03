443c6c2fc5f20a8308810aa3de1dfc0e47950512
Dan Dorman <dan.dorman@gmail.com>
2012-09-02 23:01:10 -0600

    Add global node-module path.

--- shell/path.sh
+++ shell/path.sh
@@ -2,5 +2,6 @@
 
 # system default: /usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
+PATH=$(npm bin -g 2> /dev/null):$PATH
 export PATH=./node_modules/.bin:$HOME/Code/gitflow:$HOME/bin:$PATH
 
