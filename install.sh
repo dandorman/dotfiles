96ad437f3fd1a6addc53c1979b4c0daeb552692f
Dan Dorman <dan.dorman@gmail.com>
2012-12-20 12:52:49 -0700

    Add a script for setting up symlinks.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+FILES="bash_profile gemrc gitconfig gitignore tmux.conf zshrc"
+
+for file in $FILES; do
+  link=$HOME/.$file
+
+  if [ -L $link -o ! -e $link ]; then
+    if [ -L $link ]; then
+      echo "unlinking .$file"
+      unlink $link
+    fi
+
+    echo "linking .$file"
+    ln -s "${DIR#$HOME/}/$file" "$HOME/.$file"
+
+  else
+    echo "skipping existing .$file"
+  fi
+done
