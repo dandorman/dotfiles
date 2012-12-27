78c4a6472d4a13b3e703f0aff0a449697b7a5794
Dan Dorman <dan.dorman@gmail.com>
2012-12-27 15:59:29 -0700

    Adjust PATH settings.

--- shell/path.sh
+++ shell/path.sh
@@ -5,6 +5,4 @@
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 PATH=/usr/local/heroku/bin:$PATH
 PATH=$(npm bin -g 2> /dev/null):$PATH
-PATH=$HOME/bin:$PATH
-PATH=./node_modules/.bin:$PATH
-export PATH=./bin:$PATH
+export PATH=$HOME/bin:$PATH
