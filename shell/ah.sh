ea337b8d942f3551d713d73f5e8f6974fe5fae0e
Dan Dorman <dan.dorman@gmail.com>
2013-02-03 16:07:31 -0700

    Conditionally load AH scripts.

--- shell/ah.sh
+++ shell/ah.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-export AH_PATH="$HOME/.ah-scripts"
-export AH_SCRIPT_PATH="$AH_PATH/scripts"
+path="$HOME/.ah-scripts"
+if [[ -d $path ]]; then
+  export AH_PATH=$path
+  export AH_SCRIPT_PATH="$AH_PATH/scripts"
 
-source "$AH_SCRIPT_PATH/all-scripts.sh"
-
+  source "$AH_SCRIPT_PATH/all-scripts.sh"
+fi
