500d228de91cca9b9dcceda1714376d492e317cb
Dan Dorman <dan.dorman@gmail.com>
2013-03-08 10:59:02 -0800

    Robustify `bag` in case of missing Gemfile.

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,9 +1,9 @@
 function bin_present() {
   if [[ $(which $1) = "" ]]; then
     if [[ $2 ]]; then
-      echo "no $2";
+      echo no $2
     else
-      echo "no $1";
+      echo no $1
     fi
     return 1
   fi
@@ -12,5 +12,11 @@ function bin_present() {
 function bag() {
   bin_present bundle bundler  || return $?
   bin_present ag || return $?
-  ag "$@" $(bundle show --paths)
+  paths=$(bundle show --paths)
+  if [[ $? = 10 ]]; then
+    echo no Gemfile
+    return 1
+  else
+    ag "$@" $paths
+  fi
 }
